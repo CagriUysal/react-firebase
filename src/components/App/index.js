ff6ff2436a42d1af3564a8ab435a0fcda2c5ea43
cagri <muysal.cagri@gmail.com>
2022-07-09 18:30:34 +0300

    Navigation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { ROUTES } from "../../constants/routes";
+import LandingPage from "../Landing";
+import Navigation from "../Navigation";
+import SignUpPage from "../SignUp";
+import SignInPage from "../SignIn";
+import PasswordForgetPage from "../PasswordForget";
+import HomePage from "../Home";
+import AccountPage from "../Account";
+import AdminPage from "../Admin";
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Navigation />
+      <hr />
+
+      <Routes>
+        <Route path={ROUTES.LANDING} element={<LandingPage />} />
+        <Route path={ROUTES.SIGN_UP} element={<SignUpPage />} />
+        <Route path={ROUTES.SIGN_IN} element={<SignInPage />} />
+        <Route path={ROUTES.PASSWORD_FORGET} element={<PasswordForgetPage />} />
+        <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.ACCOUNT} element={<AccountPage />} />
+        <Route path={ROUTES.ADMIN} element={<AdminPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PasswordForgetPage() {
+  return <h1>PasswordForgetPage</h1>;
+}
+
+export default PasswordForgetPage;
