b8010a0e32ead707a86793c3c39078581520653e
cagri <muysal.cagri@gmail.com>
2022-07-09 20:05:12 +0300

    firebase config

---
+++ src/components/Firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXwxUA91HFHOVupqKasN2BJ5bmKMTVNpU",
+  authDomain: "react-firebase-4dc84.firebaseapp.com",
+  projectId: "react-firebase-4dc84",
+  storageBucket: "react-firebase-4dc84.appspot.com",
+  messagingSenderId: "75084205294",
+  appId: "1:75084205294:web:a0da94954e7abd8685f7ff",
+};
+
+const app = initializeApp(firebaseConfig);
