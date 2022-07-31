5041ce37114674a4d1f4e4ed46152ed76adbf947
cagri <muysal.cagri@gmail.com>
2022-07-31 17:34:24 +0300

    add admin page

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { onSnapshot, query } from "firebase/firestore";
+import { getUsersRef } from "../Firebase/db";
 
 function AdminPage() {
-  return <h1>AdminPage</h1>;
+  const [{ users, loading }, setState] = useState({ users: [], loading: true });
+
+  useEffect(() => {
+    const q = query(getUsersRef());
+    const unsub = onSnapshot(q, (snapshot) => {
+      const users = [];
+      snapshot.forEach((userDoc) => {
+        users.push({ ...userDoc.data(), uid: userDoc.id });
+      });
+      setState({ users, loading: false });
+    });
+
+    return () => unsub();
+  }, []);
+
+  return (
+    <>
+      <h1>AdminPage</h1>
+      {loading && <div>Loading ...</div>}
+      <UserList users={users} />
+    </>
+  );
 }
+
+const UserList = ({ users }) => (
+  <ul>
+    {users.map((user) => (
+      <li key={user.uid}>
+        <span>
+          <strong>ID:</strong> {user.uid}{" "}
+        </span>
+        <span>
+          <strong>E-Mail:</strong> {user.email}{" "}
+        </span>
+        <span>
+          <strong>Username:</strong> {user.username}{" "}
+        </span>
+      </li>
+    ))}
+  </ul>
+);
 
 export default AdminPage;
--- src/components/Firebase/db.js
+++ src/components/Firebase/db.js
@@ -1,9 +1,13 @@
-import { doc, setDoc } from "firebase/firestore";
+import { collection, doc, setDoc } from "firebase/firestore";
 
 import { db } from "./config";
 
 export function getUserRef(uid) {
   return doc(db, `users/${uid}`);
+}
+
+export function getUsersRef() {
+  return collection(db, "users");
 }
 
 export function setUser(uid, data) {
